62ec2a9263e9e800f3ad64fca0150142e0852d47
Sebastian Aguirre Duque <sebastian.aguirre@guane.com.co>
2023-10-20 15:18:33 -0500

    Generate database types.

---
+++ app/types/posts.ts
@@ -0,0 +1,9 @@
+import { type Database } from '../types/database'
+
+type PostEntity = Database['public']['Tables']['posts']['Row']
+type UserEntity = Database['public']['Tables']['users']['Row']
+
+
+export type Post = PostEntity & {
+  user: UserEntity
+}
